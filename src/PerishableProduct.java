bf216743ae2c8ad633a330ae2cd123ef99354bac
Tuba Süeda Aytan <tubasueda@gmail.com>
2025-12-31 19:34:04 +0300

    PerishableProduct, constructor çağrıldı

--- src/PerishableProduct.java
+++ src/PerishableProduct.java
@@ -1,4 +1,8 @@
 public class PerishableProduct extends Product {
     private String expiryDate;
 
+    public PerishableProduct(String id, String name, double price, int quantity, String expiryDate){
+        super(id, name, price, quantity);
+        this.expiryDate = expiryDate;
+    }
 }
