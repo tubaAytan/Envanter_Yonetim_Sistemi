3b9c1dc184cab905872fa2f88e745661d8201695
Tuba Süeda Aytan <tubasueda@gmail.com>
2026-01-01 21:21:32 +0300

    Inventory, ürünleri ekrana yazdırma fonksiyonu oluşturuldu

--- src/Inventory.java
+++ src/Inventory.java
@@ -7,9 +7,19 @@ public class Inventory {
     public Inventory(){
         this.products = new ArrayList<>();
     }
-
     public void addProduct(Product product){
         products.add(product);
         System.out.println(product.getName() + " envantere başarıyla eklendi.");
+    }
+    public void listProducts(){
+        if(products.isEmpty()){
+            System.out.println("Envanterde ürün bulunmamaktadır.");
+        }
+        else{
+            System.out.println("\n~~~~ Mevcut Envanter Listesi ~~~~");
+            for(Product p: products){
+                p.displayInfo();
+            }
+        }
     }
 }
