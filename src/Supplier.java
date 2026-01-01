bf87e0b57d7efe3388ccd19bdd77a516e04461d1
Tuba Süeda Aytan <tubasueda@gmail.com>
2026-01-01 17:49:55 +0300

    Suppler getter/setter eklendi

--- src/Supplier.java
+++ src/Supplier.java
@@ -1,9 +1,24 @@
+import javax.swing.plaf.synth.SynthDesktopIconUI;
+
 public class Supplier {
     private String supplierName;
     private String contactInfo;
 
     public Supplier(String supplierName, String contactInfo){
         this.supplierName = supplierName;
+        this.contactInfo = contactInfo;
+    }
+
+    public String getSupplierName(){
+        return supplierName;
+    }
+    public void setSupplierName(String supplierName) {
+        this.supplierName = supplierName;
+    }
+    public String getContactInfo(){
+        return contactInfo;
+    }
+    public void setContactInfo(String contactInfo) {
         this.contactInfo = contactInfo;
     }
 
